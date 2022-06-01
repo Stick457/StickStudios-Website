0ca0010122f8261ef0b075f3d48f7128e57a29ea
Stick457 <76454538+Stick457@users.noreply.github.com>
2022-06-01 16:06:00 -0500

    Color update

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,11 @@
 function onload(){
-    window.location.href = "#home"};
+    window.location.href = "#home";
     window.scrollTo(0, 0);
-
+}
 window.onload(onload);
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY==0){
+        window.location.href = "#home";
+    }
+});
