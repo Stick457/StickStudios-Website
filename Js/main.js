858abbd70d2e669fd5e1cdeb424ac2a92466ffc4
Stick457 <76454538+Stick457@users.noreply.github.com>
2022-06-07 15:08:54 -0500

    update scroll

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,3 @@
-function onload(){
-    window.location.href = "#home";
-    window.scrollTo(0, 0);
-}
-window.onload(onload);
-
 window.addEventListener("scroll", function(){
     if(window.scrollY==0){
         window.location.href = "#home";
